4bf5f77f1d126d057c7b11a67e3456cdae364831
GuyTito <35842294+GuyTito@users.noreply.github.com>
2023-04-20 16:44:13 +0000

    add listview

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomeView from "./pages/HomeView";
+import ListView from "./pages/ListView";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
         <Routes>
           <Route path="/">
             <Route index element={<HomeView />} />
+            <Route path="listview" element={<ListView />} />
           </Route>
         </Routes>
       </BrowserRouter>
