3e260b920907c61fdc2a717c10375d99ba0901cc
GuyTito <35842294+GuyTito@users.noreply.github.com>
2023-04-20 18:52:25 +0000

    add card list

--- src/pages/ListView.jsx
+++ src/pages/ListView.jsx
@@ -1,12 +1,28 @@
 import styled from "styled-components";
 import Topbar from "../components/Topbar";
+import Card from "../components/Card";
 
 export default function ListView() {
   return (
     <Div>
       <Topbar />
+
+      <div className="card-list">
+        {[...Array(8).keys()].map((item, i) => (
+          <Card key={i} />
+        ))}
+      </div>
     </Div>
   );
 }
 
-const Div = styled.div``;
+const Div = styled.div`
+  .card-list {
+    margin: 0 120px;
+    margin-top: 110px;
+    display: grid;
+    grid-template-columns: repeat(4, minmax(0, 1fr));
+    gap: 52px;
+    justify-content: center;
+  }
+`;
