bbaff40539dff62bcf51f3cd5addbfbff08798f8
GuyTito <35842294+GuyTito@users.noreply.github.com>
2023-04-20 16:44:35 +0000

    Create ListView.jsx

---
+++ src/pages/ListView.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import Topbar from "../components/Topbar";
+
+export default function ListView() {
+  return (
+    <Div>
+      <Topbar />
+    </Div>
+  );
+}
+
+const Div = styled.div``;
