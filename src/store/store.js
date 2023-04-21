c054660a8afccab29d6de8b7a48ca269b8f3e69a
GuyTito <35842294+GuyTito@users.noreply.github.com>
2023-04-21 22:30:27 +0000

    add theme reducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import modalReducer from "./modalSlice";
+import themeReducer from "./themeSlice";
 
 export const store = configureStore({
   reducer: {
     modal: modalReducer,
+    theme: themeReducer,
   },
 });
