6e93058e7575e7ce462abf5d6d159aa8de07bb24
GuyTito <35842294+GuyTito@users.noreply.github.com>
2023-04-21 17:59:16 +0000

    add stats

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 import About from "./About";
+import Stats from "./Stats";
 
 export default function Content() {
   return (
     <Div>
-      <About />
-      {/* <Stats /> */}
+      {/* <About /> */}
+      <Stats />
     </Div>
   );
 }
