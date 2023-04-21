54f808aafb847b6be31baaf912344692047732b2
GuyTito <35842294+GuyTito@users.noreply.github.com>
2023-04-21 12:33:08 +0000

    Create Modal.jsx

---
+++ src/components/Modal.jsx
@@ -0,0 +1,34 @@
+import { useEffect } from "react";
+import styled from "styled-components";
+
+export default function Modal({ children, hideModal }) {
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    return () => (document.body.style.overflow = "scroll");
+  }, []);
+
+  function handleClick() {
+    hideModal();
+  }
+
+  return (
+    <Div onClick={handleClick}>
+      <div onClick={(e) => e.stopPropagation()}>{children}</div>
+    </Div>
+  );
+}
+
+const Div = styled.div`
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  z-index: 999;
+  align-items: center;
+  justify-content: center;
+  background: rgba(39, 39, 39, 0.5);
+  backdrop-filter: blur(5px);
+  overflow: hidden;
+`;
