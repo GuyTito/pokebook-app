a8e2bcf587384f7333c2c1f075112c3161a772b1
GuyTito <35842294+GuyTito@users.noreply.github.com>
2023-04-21 23:52:18 +0000

    add footer

--- src/pages/ListView.jsx
+++ src/pages/ListView.jsx
@@ -4,6 +4,7 @@ import CardList from "../components/CardList";
 import DetailView from "../components/DetailView";
 import { useSelector } from "react-redux";
 import { getDetailView } from "../store/modalSlice";
+import PageSize from "../components/PageSize";
 
 export default function ListView() {
   const showDetailView = useSelector(getDetailView);
@@ -14,10 +15,24 @@ export default function ListView() {
       <CardList />
 
       {showDetailView && <DetailView />}
+
+      <div className="footer">
+        <div>pagination</div>
+        <PageSize />
+      </div>
     </Div>
   );
 }
 
 const Div = styled.div`
+  margin: 0 120px;
   margin-bottom: 120px;
+  /* border: 1px solid red; */
+
+  .footer {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 80px;
+  }
 `;
