932dd78bca4a5e254260fedc7c8adc301cfc422c
GuyTito <35842294+GuyTito@users.noreply.github.com>
2023-04-21 18:30:02 +0000

    Create Similar.jsx

---
+++ src/components/Similar.jsx
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+import Cardlet from "./Cardlet";
+
+export default function Similar() {
+  return (
+    <Div>
+      <h2>Similar</h2>
+
+      <div className="content">
+        <Cardlet />
+        <Cardlet />
+      </div>
+    </Div>
+  );
+}
+
+const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h2 {
+    width: 510px;
+    padding: 12px;
+    font-family: var(--font-clash);
+    font-weight: 600;
+    font-size: 24px;
+    line-height: 30px;
+    text-align: center;
+    box-shadow: 0px -28px 24px -24px rgba(0, 0, 0, 0.25);
+  }
+
+  .content {
+    width: 510px;
+    display: flex;
+    justify-content: center;
+    gap: 10px;
+    background: linear-gradient(
+      90deg,
+      #ffffff 0%,
+      rgba(233, 233, 233, 0.479167) 52.08%,
+      #ffffff 100%
+    );
+  }
+`;
