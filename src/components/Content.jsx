edca7a234c076c025772e04ba2cf6f039bb500d3
GuyTito <35842294+GuyTito@users.noreply.github.com>
2023-04-21 17:40:26 +0000

    Create Content.jsx

---
+++ src/components/Content.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import About from "./About";
+
+export default function Content() {
+  return (
+    <Div>
+      <About />
+      {/* <Stats /> */}
+    </Div>
+  );
+}
+
+const Div = styled.div``;
