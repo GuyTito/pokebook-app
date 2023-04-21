7f6e2a202d8fe5a733304e96f736fc2803eda663
GuyTito <35842294+GuyTito@users.noreply.github.com>
2023-04-21 18:29:52 +0000

    Create Cardlet.jsx

---
+++ src/components/Cardlet.jsx
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+import Charizard from "../assets/charizard.svg";
+
+export default function Cardlet() {
+  return (
+    <Div>
+      <div className="cover">
+        <img src={Charizard} alt="" />
+      </div>
+
+      <h3>charizard</h3>
+    </Div>
+  );
+}
+
+const Div = styled.div`
+  background: white;
+  width: 220px;
+  height: 169px;
+  box-shadow: 0px 4px 40px rgba(0, 0, 0, 0.06);
+  border-radius: 20px;
+  padding: 0 7px;
+  padding-top: 7px;
+  position: relative;
+  margin-top: 52px;
+  margin-bottom: 14px;
+
+  .cover {
+    background-color: #f1f1f1;
+    width: 100%;
+    height: 113px;
+    border-radius: 15px;
+    position: relative;
+
+    img {
+      position: absolute;
+      top: -55px;
+      left: 30px;
+      height: 137.62582397460938px;
+      width: 137.92498779296875px;
+    }
+  }
+
+  h3 {
+    font-family: var(--font-clash);
+    font-weight: 500;
+    font-size: 24px;
+    line-height: 30px;
+    text-align: center;
+    margin: 8px 0;
+  }
+`;
