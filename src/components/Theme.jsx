d013ab635fe74b5fa0dee2ca2bc4cbdbd8c55511
GuyTito <35842294+GuyTito@users.noreply.github.com>
2023-04-21 22:34:41 +0000

    Create Theme.jsx

---
+++ src/components/Theme.jsx
@@ -0,0 +1,25 @@
+import { useDispatch, useSelector } from "react-redux";
+import styled from "styled-components";
+import { getThemeModal, showThemeModal } from "../store/modalSlice";
+import ThemeModal from "./ThemeModal";
+import TButton from "./TButton";
+import { getThemeColor } from "../store/themeSlice";
+
+export default function Theme() {
+  const displayThemeModal = useSelector(getThemeModal);
+  const themeColor = useSelector(getThemeColor);
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <TButton
+        handleFunction={() => dispatch(showThemeModal())}
+        bgColor={themeColor}
+      />
+
+      {displayThemeModal && <ThemeModal />}
+    </>
+  );
+}
+
+const Div = styled.div``;
