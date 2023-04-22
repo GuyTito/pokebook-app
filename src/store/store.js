00ef300da484e5e23881acf2108db3251b5209a3
GuyTito <35842294+GuyTito@users.noreply.github.com>
2023-04-22 17:11:16 +0000

    add pokemon reducer

--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import modalReducer from "./modalSlice";
 import themeReducer from "./themeSlice";
 import pageReducer from "./pageSlice";
+import pokemonReducer from "./pokemonSlice";
 
 export const store = configureStore({
   reducer: {
     modal: modalReducer,
     theme: themeReducer,
     page: pageReducer,
+    pokemon: pokemonReducer,
   },
 });
