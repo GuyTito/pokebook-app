026bffcd5f3a0a3627909a5fad8529f3b059cf8e
GuyTito <35842294+GuyTito@users.noreply.github.com>
2023-04-22 08:07:30 +0000

    change page size

--- src/components/PageSize.jsx
+++ src/components/PageSize.jsx
@@ -1,59 +1,90 @@
 import styled from "styled-components";
 import DownIcon from "../assets/down_icon.svg";
-import { useState } from "react";
+import { changePageSize, getPageSize } from "../store/pageSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function PageSize() {
-  const [showDropdown, setShowDropdown] = useState(false);
+  const pageSize = useSelector(getPageSize);
+  const dispatch = useDispatch();
+  let sizes = [8, 12, 16, 24];
+
+  function handleClick(size) {
+    dispatch(changePageSize(size));
+  }
+
   return (
     <Div onClick={() => setShowDropdown(!showDropdown)}>
-      <div className="size">8</div>
-      <img src={DownIcon} alt="down icon" />
+      <div className="page-size">
+        <div className="size">{pageSize}</div>
+        <img src={DownIcon} alt="down icon" />
 
-      {showDropdown && (
         <ul className="content">
-          <li style={{ background: "#F3F3F3" }}>12</li>
-          <li>16</li>
-          <li>24</li>
+          {sizes.map((size) => (
+            <li
+              onClick={() => handleClick(size)}
+              style={size == pageSize ? { display: "none" } : {}}
+            >
+              {size}
+            </li>
+          ))}
         </ul>
-      )}
+      </div>
     </Div>
   );
 }
 
 const Div = styled.div`
-  font-family: var(--font-clash);
-  font-weight: 500;
-  font-size: 18px;
-  line-height: 22px;
-  display: flex;
-  padding: 6px 8px;
-  align-items: center;
-  background: #e1e1e1;
-  border-radius: 8px;
-  width: fit-content;
-  gap: 10px;
-  cursor: pointer;
-  position: relative;
-
-  .size {
+  .page-size {
+    font-family: var(--font-clash);
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 22px;
+    display: flex;
     padding: 6px 8px;
+    align-items: center;
+    background: #e1e1e1;
+    border-radius: 8px;
+    width: fit-content;
+    gap: 10px;
+    cursor: pointer;
     position: relative;
-    background: #ffffff;
-    border-radius: 4px;
+
+    .size {
+      padding: 6px 8px;
+      position: relative;
+      background: #ffffff;
+      border-radius: 4px;
+    }
+
+    .content {
+      position: absolute;
+      background: #ffffff;
+      border-radius: 8px;
+      right: 0;
+      bottom: 0;
+      transform: translateY(99%);
+      padding: 5px;
+      margin-top: 2px;
+      opacity: 0;
+      transition: all 0.3s ease;
+
+      li {
+        padding: 5px 30px;
+        border-radius: 6px;
+        display: none;
+      }
+
+      li:hover {
+        background-color: #f3f3f3;
+      }
+    }
   }
 
-  .content {
-    position: absolute;
-    background: #ffffff;
-    border-radius: 8px;
-    top: 100%;
-    right: 0;
-    padding: 5px;
-    margin-top: 2px;
+  .page-size:hover .content {
+    opacity: 1;
+  }
 
-    li {
-      padding: 5px 30px;
-      border-radius: 6px;
-    }
+  .page-size:hover li {
+    display: block;
   }
 `;
