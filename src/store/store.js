079e2e477f40569624c4f7744be0e64bcff72482
GuyTito <35842294+GuyTito@users.noreply.github.com>
2023-04-22 08:06:45 +0000

    add page reducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import modalReducer from "./modalSlice";
 import themeReducer from "./themeSlice";
+import pageReducer from "./pageSlice";
 
 export const store = configureStore({
   reducer: {
     modal: modalReducer,
     theme: themeReducer,
+    page: pageReducer,
   },
 });
