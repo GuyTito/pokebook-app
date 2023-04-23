e8f77b2964321e7bd09a8d02e945624570229604
GuyTito <35842294+GuyTito@users.noreply.github.com>
2023-04-23 13:33:45 +0000

    feat: Add useViewportWidth hook to track viewport width
    This commit adds a new custom React hook called `useViewportWidth`, which tracks the current width of the viewport and updates it whenever the viewport is resized. The hook uses the `useState` and `useEffect` hooks to manage state and add event listeners, respectively.
    
    The `useViewportWidth` hook can be used in any React component to retrieve the current viewport width and use it to update the UI as needed. This adds a simple and reusable way to make a component responsive to changes in viewport size.

---
+++ src/utils/useViewportWidth.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+
+export default function useViewportWidth() {
+  const [viewportWidth, setViewportWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setViewportWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return viewportWidth;
+}
