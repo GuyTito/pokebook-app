718be60eb05457f4136f179409d2255243da82e2
GuyTito <35842294+GuyTito@users.noreply.github.com>
2023-04-23 13:35:15 +0000

    feat: add media query break points

---
+++ src/utils/devices.js
@@ -0,0 +1,5 @@
+export const lsm = `(max-width: 640px)`; // less than sm
+export const sm = `(min-width: 640px)`;
+export const md = `(min-width: 768px)`;
+export const lg = `(min-width: 1024px)`;
+export const xl = `(min-width: 1440px)`;
