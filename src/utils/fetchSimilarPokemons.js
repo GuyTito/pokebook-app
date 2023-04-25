6587d32c0ac939016a6a6a4f24d810d8837a48d6
GuyTito <35842294+GuyTito@users.noreply.github.com>
2023-04-25 02:16:59 +0000

    Update fetchSimilarPokemons.js

--- src/utils/fetchSimilarPokemons.js
+++ src/utils/fetchSimilarPokemons.js
@@ -1,9 +1,9 @@
 import fetchData from "./fetchData";
 
-export async function fetchSimilarPokemons(arrayObjs) {
-  let types = arrayObjs.map((item) => item?.type?.name);
+export async function fetchSimilarPokemons(creatureTypes) {
+  let types = creatureTypes?.map((item) => item?.type?.name);
   let typeObjects = await Promise.all(
-    types.map((type) => fetchData(`https://pokeapi.co/api/v2/type/${type}`))
+    types?.map((type) => fetchData(`https://pokeapi.co/api/v2/type/${type}`))
   );
   let pokemonsArr = typeObjects.map((type) => type.pokemon.slice(3, 5));
   let urls = pokemonsArr.flat().map((obj) => obj.pokemon.url);
